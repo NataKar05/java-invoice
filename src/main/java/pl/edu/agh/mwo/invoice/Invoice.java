9133bc2e098171cdf8709832434a7350aab23e4d
nkarwala <natalia.karvala@gmail.com>
2025-02-08 12:09:12 +0100

    Invoice tests updated, to be continued as homework

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,40 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private Map<Product, Integer> productIndex= new HashMap<>();
+
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        for(int i=0; i < quantity; i++) {
+            addProduct(product);
+        }
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetPrice() {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Product product : products) {
+            sum = sum.add(product.getPrice());
+        }
+        return sum;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return BigDecimal.ZERO;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossPrice() {
+        return BigDecimal.ZERO;
     }
 }
